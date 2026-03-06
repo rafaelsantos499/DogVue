a836f2ed7a68eda0d56ca0594bdfbb53ad43261d
Rafael santos <rafaelsantos499@gmail.com>
2026-03-05 22:51:40 -0300

    Feat: footer styler

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,31 @@
 <template>
-  <div>Footer</div>
+  <footer class="footer">
+    <svg width="28" height="22" viewBox="0 0 28 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path fill-rule="evenodd" clip-rule="evenodd" d="M14 10h1.652c1.708 0 2.63 2.004 1.518 3.302l-1.618 1.887A4.501 4.501 0 0024.5 14.5a1.5 1.5 0 013 0A7.5 7.5 0 0114 19 7.5 7.5 0 01.5 14.5a1.5 1.5 0 013 0 4.5 4.5 0 008.948.689l-1.618-1.887C9.718 12.004 10.64 10 12.35 10H14z" fill="#764701"/>
+      <circle cx="21" cy="3" r="3" fill="#764701"/>
+      <circle cx="7" cy="3" r="3" fill="#764701"/>
+    </svg>
+    <p>Dogs. Alguns direitos reservados.</p>
+  </footer>
 </template>
-<script lang="ts">
-export default {};
+
+<script setup lang="ts">
 </script>
-<style lang=""></style>
+
+<style scoped>
+.footer {
+  background: #fb1;
+  padding: 3rem 1rem 0 1rem;
+  height: 10rem;
+  text-align: center;
+  color: #764701;
+}
+
+.footer p {
+  margin-top: 1rem;
+}
+
+.footer img {
+  margin: 0 auto;
+}
+</style>
